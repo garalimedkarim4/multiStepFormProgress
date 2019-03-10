f29a0123d8a0659fa5cad07a41791d6c4eaf285c
Garali Mohamed Karim <garalimedkarim2@gmail.com>
2019-03-10 21:19:29 +0100

    adapted in angular

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ console.log("ready");
 var current_fs, next_fs, previous_fs; //fieldsets
 var left, opacity, scale; //fieldset properties which we will animate
 var animating; //flag to prevent quick multi-click glitches
-
+/*
 $(".next").click(function(){ console.log('.next');
 	if(animating) return false;
 	animating = true;
@@ -13,6 +13,7 @@ $(".next").click(function(){ console.log('.next');
 	current_fs = $(this).parent();
 	next_fs = $(this).parent().next();
 
+console.log("3",current_fs,next_fs);
 	//activate next step on progressbar using the index of next_fs
 	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
 
@@ -43,6 +44,8 @@ $(".next").click(function(){ console.log('.next');
 		easing: 'easeInOutBack'
 	});
 });
+
+
 
 $(".previous").click(function(){
 	if(animating) return false;
@@ -78,7 +81,7 @@ $(".previous").click(function(){
 		easing: 'easeInOutBack'
 	});
 });
-
+*/
 $(".submit").click(function(){
 	return false;
 })
@@ -92,5 +95,49 @@ app.controller('myCtrl', function($scope) {
   $scope.change = function(){
     console.log('change2');
 
+  };
+	var current_fs, next_fs, previous_fs; //fieldsets
+	var left, opacity, scale; //fieldset properties which we will animate
+	var animating; //flag to prevent quick multi-click glitches
+
+  $scope.navAction = function(current,next){
+		console.log('step1');
+		if(animating) return false;
+		animating = true;
+
+		current_fs = $(current);
+		next_fs = $(next);
+
+		//Progress Baaaaar:
+		//activate next step on progressbar using the index of next_fs
+		// $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+
+		//show the next fieldset
+		next_fs.show();
+		console.log("shoooow");
+		//hide the current fieldset with style
+		current_fs.animate({opacity: 0}, {
+			step: function(now, mx) {
+				//as the opacity of current_fs reduces to 0 - stored in "now"
+				//1. scale current_fs down to 80%
+				scale = 1 - (1 - now) * 0.2;
+				//2. bring next_fs from the right(50%)
+				left = (now * 50)+"%";
+				//3. increase opacity of next_fs to 1 as it moves in
+				opacity = 1 - now;
+				current_fs.css({
+	        'transform': 'scale('+scale+')',
+	        'position': 'absolute'
+	      });
+				next_fs.css({'left': left, 'opacity': opacity});
+			},
+			duration: 800,
+			complete: function(){
+				current_fs.hide();
+				animating = false;
+			},
+			//this comes from the custom easing plugin
+			easing: 'easeInOutBack'
+		});
   };
 });
